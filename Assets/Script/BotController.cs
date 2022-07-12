16909d49fc430d932883dfd2b0a35576b56f7b47
crossingbyX0 <tungpahe150361@fpt.edu.vn>
2022-07-12 15:27:48 +0700

    tungWithTrap

--- Assets/Script/BotController.cs
+++ Assets/Script/BotController.cs
@@ -5,16 +5,40 @@ using UnityEngine;
 using UnityEngine.AI;
 using Random = UnityEngine.Random;
 
-public class BotController : MonoBehaviour
+public class BotController : EnemyDamage
 {
+    //[HideInInspector]
+    //public bool mustPatrol = false;
+
     public float walkSpeed = 2f;
     public bool moveRight = true;
     public Transform groundDetection;
     public LayerMask groundLayer;
     public Collider2D collider;
+    public Rigidbody2D rb;
+    [SerializeField]
+    public Transform player;
+    SpriteRenderer sprite;
+    [SerializeField]
+    public float argoRange;
+
+    private void Start()
+    {
+        //mustPatrol = true;
+    }
     void Update()
     {
+    
         EnemyMovement();
+        float disToPlayer = Vector2.Distance(transform.position, player.position);
+        if (disToPlayer < argoRange)
+        {
+            ChasePlayer();
+        }
+        else
+        {
+            StopChasing();
+        }
     }
     void EnemyMovement()
     {
@@ -26,13 +50,54 @@ public class BotController : MonoBehaviour
             {
                 transform.eulerAngles = new Vector3(0, -180, 0);
                 moveRight = false;
+               
             }
             else
             {
+                
                 transform.eulerAngles = new Vector3(0, 0, 0);
                 moveRight = true;
+              
             }
         }
     }
+    void ChasePlayer()
+    {
+        RaycastHit2D groundInfo = Physics2D.Raycast(groundDetection.position, Vector2.down, 2f);
+        if (transform.position.x < player.position.x
+            && (!collider.IsTouchingLayers(groundLayer)
+            || groundInfo.collider == true))
+        {
+            rb.velocity = new Vector2(walkSpeed + 1, 0);
+            transform.eulerAngles = new Vector3(0, 0, 0);
+        }
+        else if (transform.position.x > player.position.x)
+        {
+            rb.velocity = new Vector2(-walkSpeed - 1, 0);
+            transform.eulerAngles = new Vector3(0, -180, 0);
+        }
+    }
+    void StopChasing()
+    {
+        RaycastHit2D groundInfo = Physics2D.Raycast(groundDetection.position, Vector2.down, 2f);
+        if (transform.position.x == player.position.x
+           && (!collider.IsTouchingLayers(groundLayer)
+           || groundInfo.collider == false))
+        {
+            rb.velocity = new Vector2(walkSpeed, 0);
+            transform.eulerAngles = new Vector3(0, 0, 0);
+           
+        }
+        else
+        {
+
+        }
+
+    }
+    void DamagePlayer()
+    {
+
+    }
+
 
 }
