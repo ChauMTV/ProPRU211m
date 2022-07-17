8777b9da140a37167a81d5c29e61038e89e634f3
WacMac113 <unknowquest@yahoo.com>
2022-07-17 23:07:09 +0700

    up date to main

---
+++ Assets/Script/HUD.cs
@@ -0,0 +1,60 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+
+/// <summary>
+/// A HUD
+/// </summary>
+public class HUD : MonoBehaviour
+{
+    #region Fields
+
+    // scoring support
+    [SerializeField]
+    TextMeshProUGUI scoreText;
+    [SerializeField]
+    TextMeshProUGUI coinText;
+
+    int score;
+    int coin;
+
+
+    const string ScorePrefix = "Score: ";
+    const string coins = "Coin: ";
+  
+
+
+    #endregion
+
+    /// <summary>
+    /// Start is called before the first frame update
+    /// </summary>	
+    void Start()
+    {
+        coinText.text = coins + score.ToString();
+        scoreText.text = ScorePrefix + score.ToString();
+        
+       
+        //scoreText.color = Color.white;
+    }
+
+    /// <summary>
+    /// Adds the given points to the score
+    /// </summary>
+    /// <param name="points">points</param>
+    public void AddPoints(int points)
+    {
+        score += points;
+        scoreText.text = ScorePrefix + score.ToString();
+    }
+    public void AddCoin(int points)
+    {
+        coin += points;
+        coinText.text = coins + coin.ToString();
+    }
+
+
+
+}
---
+++ Assets/Script/MainMenuController.cs
@@ -0,0 +1,39 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenuController : MonoBehaviour
+
+{
+    
+
+    public void Playgame()
+    {
+        SceneManager.LoadScene("LevelSelect");
+    }
+    public void Quitgame()
+    {
+        SceneManager.LoadScene("Scense0");
+    }
+
+    public void Slectmap()
+    {
+        SceneManager.LoadScene("Scense0");
+    }
+
+    public void Slectmap1()
+    {
+        SceneManager.LoadScene("SceneTung");
+    }
+
+    public void Slectmap2()
+    {
+        SceneManager.LoadScene("ScenceHuyen");
+    }
+
+    public void BacktoMenu()
+    {
+        SceneManager.LoadScene("MainMenu");
+    }
+}
