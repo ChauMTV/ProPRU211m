ae20415ee34d06a25ec9c784af1e1b37fbf9d18f
crossingbyX0 <tungpahe150361@fpt.edu.vn>
2022-07-19 14:15:59 +0700

    tungbotcoinsort

---
+++ Assets/Script/Item/DamagePowerUp.cs
@@ -0,0 +1,25 @@
+using UnityEngine;
+
+public class DamagePowerUp : MonoBehaviour
+{
+    //bool for check when player collide with item powerup
+    public bool activated;
+    
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+
+        if (collision.tag == "Player")
+        {
+            GameObject player = collision.gameObject;
+            //change the weapon to instantiate a poweredup bullet
+            Weapon bullet = player.GetComponent<Weapon>();
+            activated = true;
+            bullet.isPoweredUp = activated;
+            Destroy(gameObject);
+
+        
+        }
+       
+
+    }
+}
