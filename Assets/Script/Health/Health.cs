16c07df98ce9ea267f7f20af0c5e4098f833393d
yamisf000 <tuyennhhe140313@fpt.edu.vn>
2022-07-21 11:04:30 +0700

    boss feature

--- Assets/Script/Health/Health.cs
+++ Assets/Script/Health/Health.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 using UnityEngine.UI;
 
 public class Health : MonoBehaviour
@@ -19,6 +20,12 @@ public class Health : MonoBehaviour
     [SerializeField]
     private int numberOfFlashes;
     private SpriteRenderer spriteRenderer;
+
+    private bool invulnerable;
+    [Header("Death")]
+    [SerializeField] private AudioClip deathSoundEffect;
+    [Header("Hurt")]
+    [SerializeField] private AudioClip hurtSoundEffect;
     public float currenHealth { get; private set; }
     // Start is called before the first frame update
 
@@ -30,12 +37,18 @@ public class Health : MonoBehaviour
     }
     public void TakeDamage(float _damage)
     {
+        if (invulnerable )
+        {
+            return;
+        }
         currenHealth = Mathf.Clamp(currenHealth - _damage, 0, startingHealth);
         if (currenHealth > 0)
         {
             //hurt
+            
             anim.SetTrigger("hurt");
             StartCoroutine(Invunerability());
+            SoundManager.instance.PlaySound(hurtSoundEffect);
             //iframe
 
         }
@@ -44,16 +57,41 @@ public class Health : MonoBehaviour
             //die
             if (!dead)
             {
+                
                 anim.SetTrigger("die");
                 GetComponent<CharacterMovement>().enabled = false;
                 GetComponent<Weapon>().enabled = false;
                 dead = true;
+                SoundManager.instance.PlaySound(deathSoundEffect);
             }
-
+            
+          
         }
+        //if (currenHealth==0)
+        //{
+        //    SceneManager.LoadScene("GamoverScense");
+        //}
     }
+
+    public void AddHealth(float _value)
+    {
+        currenHealth = Mathf.Clamp(currenHealth + _value, 0, startingHealth);
+    }
+
+    public void Respawn()
+    {
+        dead = false;
+        AddHealth(startingHealth);
+        anim.ResetTrigger("die");
+        anim.Play("Idle");
+        StartCoroutine(Invunerability());
+        GetComponent<CharacterMovement>().enabled = true;
+        GetComponent<Weapon>().enabled = true;
+    }
+
     private IEnumerator Invunerability()
     {
+        invulnerable = true;
         Physics2D.IgnoreLayerCollision(10, 11,true);
         //thoi gian bat tu 
         for (int i = 0; i < numberOfFlashes; i++)
@@ -64,5 +102,6 @@ public class Health : MonoBehaviour
             yield return new WaitForSeconds(IFrameDuration / (numberOfFlashes * 2));
         }
         Physics2D.IgnoreLayerCollision(10, 11, false);
+        invulnerable=false;
     }
 }
---
+++ Assets/Script/Player/BulletScript.cs
@@ -0,0 +1,103 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class BulletScript : MonoBehaviour
+{
+    public float speed = 1f;
+    public Rigidbody2D rb;
+    public bool isBossTakenDmg;
+
+
+
+
+
+    //start destroy
+    #region Fields
+
+    [SerializeField]
+    GameObject prefabExplosion;
+
+    public int damage = 50;
+
+    #endregion
+
+    #region Properties
+
+    /// <summary>
+    /// Gets the damage the fish inflicts
+    /// </summary>
+    public int Damage
+    {
+        get { return damage; }
+    }
+
+    #endregion
+
+    #region Methods
+
+
+    /// <summary>
+    /// Destroy moster on collision
+    /// </summary>
+    /// <param name="collision">collision info</param>
+    private void OnCollisionEnter2D(Collision2D collision)
+    {   
+        if (collision.gameObject.CompareTag("monster"))
+        {
+            Instantiate<GameObject>(prefabExplosion,
+                transform.position, Quaternion.identity);
+           
+            Destroy(gameObject);
+
+        }
+        Destroy(gameObject);
+    }
+    /// <summary>
+    /// Destroy when leave game
+    /// </summary>
+    void OnBecameInvisible()
+    {
+
+        Destroy(gameObject);
+    }
+
+    #endregion
+  private void OnTriggerEnter2D(Collider2D collision)
+    {
+        Debug.Log(collision.name);
+        if (collision.gameObject.CompareTag("TBoss"))
+        {
+ 
+            collision.GetComponent<BossT>().TakeHit(1);
+            isBossTakenDmg = true;
+
+        }
+        if (!collision.gameObject.CompareTag("FireBall"))
+        {
+            Destroy(gameObject);
+        }
+       
+    }
+
+    //end destroy 
+
+    public void Awake()
+    {
+
+        isBossTakenDmg = false;
+    }
+
+   
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+    public void FixedUpdate()
+    {
+        rb.velocity = transform.right * speed;
+    }
+}
